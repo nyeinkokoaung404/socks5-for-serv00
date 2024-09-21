96a22e7959852158c9295f4c0f513529a26228b8
4-0-4 <146018190+nyeinkokoaung404@users.noreply.github.com>
2024-09-21 06:36:06 +0630

    check_cron.sh

---
+++ check_cron.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+USER=$(whoami)
+WORKDIR="/home/${USER}/.nezha-agent"
+FILE_PATH="/home/${USER}/.s5"
+CRON_S5="nohup ${FILE_PATH}/s5 -c ${FILE_PATH}/config.json >/dev/null 2>&1 &"
+CRON_NEZHA="nohup ${WORKDIR}/start.sh >/dev/null 2>&1 &"
+PM2_PATH="/home/${USER}/.npm-global/lib/node_modules/pm2/bin/pm2"
+CRON_JOB="*/12 * * * * $PM2_PATH resurrect >> /home/$(whoami)/pm2_resurrect.log 2>&1"
+REBOOT_COMMAND="@reboot pkill -kill -u $(whoami) && $PM2_PATH resurrect >> /home/$(whoami)/pm2_resurrect.log 2>&1"
+
+echo "Check and add crontab tasks"
+
+if [ "$(command -v pm2)" == "/home/${USER}/.npm-global/bin/pm2" ]; then
+  echo "PM2 has been installed and the correct path has been returned. The PM2 keepalive task is enabled."
+  (crontab -l | grep -F "$REBOOT_COMMAND") || (crontab -l; echo "$REBOOT_COMMAND") | crontab -
+  (crontab -l | grep -F "$CRON_JOB") || (crontab -l; echo "$CRON_JOB") | crontab -
+else
+  if [ -e "${WORKDIR}/start.sh" ] && [ -e "${FILE_PATH}/config.json" ]; then
+    echo "Add crontab restart task for nezha & socks5"
+    (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && ${CRON_S5} && ${CRON_NEZHA}") || (crontab -l; echo "@reboot pkill -kill -u $(whoami) && ${CRON_S5} && ${CRON_NEZHA}") | crontab -
+    (crontab -l | grep -F "* * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}") || (crontab -l; echo "*/12 * * * * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}") | crontab -
+    (crontab -l | grep -F "* * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") || (crontab -l; echo "*/12 * * * * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") | crontab -
+  elif [ -e "${WORKDIR}/start.sh" ]; then
+    echo "Add a crontab restart task for nezha"
+    (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && ${CRON_NEZHA}") || (crontab -l; echo "@reboot pkill -kill -u $(whoami) && ${CRON_NEZHA}") | crontab -
+    (crontab -l | grep -F "* * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}") || (crontab -l; echo "*/12 * * * * pgrep -x \"nezha-agent\" > /dev/null || ${CRON_NEZHA}") | crontab -
+  elif [ -e "${FILE_PATH}/config.json" ]; then
+    echo "Add socks5 crontab restart task"
+    (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && ${CRON_S5}") || (crontab -l; echo "@reboot pkill -kill -u $(whoami) && ${CRON_S5}") | crontab -
+    (crontab -l | grep -F "* * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") || (crontab -l; echo "*/12 * * * * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") | crontab -
+  fi
+fi
